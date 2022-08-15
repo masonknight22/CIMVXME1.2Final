d47309c5dc8ccbf4d36ff0c1e6e1b13c2a30f753
Mason Knight <92546628+masonknight22@users.noreply.github.com>
2022-08-15 05:00:08 -0400

    Add files via upload

---
+++ Final.c
@@ -0,0 +1,23 @@
+task main()
+{
+    while(1==1)
+    {
+        int valTime1, valTime10, valTime100;
+        untilSonarLessThan(10, dgt12);
+        writeDebugStreamLine("int x is: %d", valTime1 = time1[T1]);
+        clearTimer (t1);
+        turnLEDOn (dgtl11);
+        wait10Msec(10);
+        turnLEDOff (dgtl11);
+        untilSonarLessThan (20, dgtl2);
+        valTime1=time1[T1];
+
+        valTime10=time10[T1];
+
+        valTime100=time100[T1];
+
+        turnLEDOn (dgtl11);
+        wait10Msec(10);
+        turnLEDOff(dgtl11);
+    }
+}
