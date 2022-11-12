8016034fca35bec054ac6a966f58fd77b0a92389
Mason Knight <92546628+masonknight22@users.noreply.github.com>
2022-11-12 03:15:10 -0500

    Add files via upload

---
+++ robot.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+task e_stop()
+{
+    while(true)
+    {
+        if(SensorValue(e_sopBtn) == 1)
+        {
+            stopAllTasks();    //ends the program andall task main.
+        }
+        wait1Msec(10);
+    }
+}
+
+
+////////////////////////////FAIL METHOD, STATEMENT STATEMENT///////////////////////////////
+
+ifstream in("final.c");
+if (!in == true)
+{
+    final.fail();
+}
+else 
+{
+    
+}
+
+infile.open("robot5.c");
+if(infile.fail())
+{
+    cout<<"Mechanical Failure. Emergency stop deployed. Please reset the system."
+}
+else{
+    while(task main)
+}
+
+return 0;
